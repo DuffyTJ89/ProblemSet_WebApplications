e962ceabae90b4a08b6ff95d470e1eb7b332d69c
DuffyTJ89 <tomduffy89@gmail.com>
2017-10-26 18:21:27 +0100

    added Q4

--- GuessingGame.go
+++ GuessingGame.go
@@ -9,15 +9,22 @@ import (
 	"net/http"
 )
 
-
+//root request
 func server(w http.ResponseWriter, r *http.Request){
-		http.ServeFile(w, r, "guess.html")
+		http.ServeFile(w, r, "index.html")
 	}
+
+// /guess request
+func guessHandler(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "guess.html")
+}
+
 
 
 func main() {
 	http.HandleFunc("/", server)//"/" handles any requests and passes to server
-	
+	http.HandleFunc("/guess", guessHandler) //handles requests for guess
+
 	log.Println("Listening....")
     http.ListenAndServe(":8080", nil)// serve on port 8080
 }
